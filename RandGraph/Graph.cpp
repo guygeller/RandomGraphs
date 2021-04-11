acad23e3c5247a534cc1fda79e04c1082bc74ff0
Guy Geller <61310269+guygeller@users.noreply.github.com>
2021-04-11 09:32:22 +0300

    Graph from SET to VECTOR, LIST in BFS to QUEUE

--- RandGraph/Graph.cpp
+++ RandGraph/Graph.cpp
@@ -1,16 +1,14 @@
 ﻿#include<iostream>
-#include<list>
-#include<set>
-#include<vector>
-#include<algorithm>
 #include<fstream>
-#include<cmath>
-#define INF 2 << 22 // ~~830k
+#include<vector>
+#include<queue>
+#include<algorithm>
+#define INF INT_MAX
 using namespace std;
 
 struct Graph {
 	int V; // count of the nodes in the graph
-	set<int, greater<int> >* adjList; // neighbors
+	vector<int>* adjList; // neighbors
 };
 
 //fucntions:
@@ -20,11 +18,9 @@ Graph* build_random_graph(int V, float p);
 int diameter(Graph* graph);
 bool Is_Isolated(Graph* graph);
 bool connectivity(Graph* graph);
-//void create();
 int Test1(int V, int itr, float P);
 int Test2(int V, int itr, float P);
 int Test3(int V, int itr, float P);
-
 
 
 // A utility function that creates a graph of V vertices
@@ -34,7 +30,7 @@ Graph* createGraph(int V) {
 
 	// Create an array of sets representing
 	// adjacency lists.  Size of the array will be V
-	graph->adjList = new set<int, greater<int> >[V];
+	graph->adjList = new vector<int>[V];
 
 	return graph;
 }
@@ -44,44 +40,12 @@ void addEdge(Graph* graph, int src, int dest) {
 	// Add an edge from src to dest.  A new
 	// element is inserted to the adjacent
 	// list of src.
-	graph->adjList[src].insert(dest);
+	graph->adjList[src].push_back(dest);
 
 	// Since graph is undirected, add an edge
 	// from dest to src also
-	graph->adjList[dest].insert(src);
+	graph->adjList[dest].push_back(src);
 }
-
-
-//A utility function to print the adjacency
-//list representation of graph
-//void printGraph(Graph* graph)
-//{
-//	for (int i = 0; i < graph->V; ++i) {
-//		set<int, greater<int> > lst = graph->adjList[i];
-//		cout << i << ": ";
-//		for (auto itr = lst.begin(); itr != lst.end(); ++itr)
-//			cout << *itr << " ";
-//		cout << endl;
-//	}
-//
-//}
-
-//Searches for a given edge in the graph
-//int searchEdge(Graph* graph, int src, int dest)
-//{
-//	auto itr = graph->adjList[src].find(dest);
-//
-//	if (itr == graph->adjList[src].end())
-//		/*cout << endl << "Edge from " << src
-//		<< " to " << dest << " not found."
-//		<< endl;*/
-//		return 0;
-//	else
-//		/*cout << endl << "Edge from " << src
-//		<< " to " << dest << " found."
-//		<< endl;*/
-//		return 1;
-//}
 
 
 Graph* build_random_graph(int V, float P) {
@@ -107,9 +71,7 @@ int diameter(Graph* graph) {
 		// initialising the start vertex
 		visited[i] = true;
 		distance[i] = 0;
-
 		BFS(graph, i, visited, distance);
-
 		// distance from the start vertex to the farthest leaf
 		int max_dist = *max_element(distance.begin(), distance.end());
 		if (max_dist == INF)
@@ -136,9 +98,7 @@ bool connectivity(Graph* graph) {
 	// initialising the start vertex
 	visited[0] = true;
 	distance[0] = 0;
-
 	BFS(graph, 0, visited, distance);
-
 	//stating from the second vertex
 	for (int i = 1; i < graph->V; i++) {
 		if (!visited[i]) // if not visited that means BFS fucntion hasn't reached all the nodes
@@ -148,46 +108,23 @@ bool connectivity(Graph* graph) {
 }
 
 void BFS(Graph* graph, int startVertex, vector<bool>& visited, vector<int>& distance) {
-	/*
-	// resize the vectors (visited and distance arrays) and intialise with false
-	visited.resize(graph->V, false);
-	distance.resize(graph->V, INF);
-
-	//// initialising the start vertex
-	distance[startVertex] = 0;
-	visited[startVertex] = true;
-	*/
-
 	//create queue for bfs and push the start vertex
-	list<int> queue;
-	queue.push_back(startVertex);
-
+	queue<int> queue;
+	queue.push(startVertex);
 	while (!queue.empty()) {
 		startVertex = queue.front();
-		queue.pop_front();
 		// for each neighbors of 'i' vertex
 		for (auto i : graph->adjList[startVertex]) {
 			// mark as visited, push into the queue, save distance from starting node
 			if (!visited[i]) {
 				visited[i] = true;
-				queue.push_back(i);
+				queue.push(i);
 				distance[i] = distance[startVertex] + 1;
 			}
 		}
+		queue.pop();
 	}
 }
-
-//void create()//write to CSV file
-//{
-//	ofstream file;
-//	// opens an existing csv file or creates a new file.
-//	file.open("Test.csv");
-//	for (int i = 0; i < 10; i++) { // 10 tests for each P value
-//		// Insert the data to file
-//		file << endl;
-//	}
-//	file.close();
-//}
 
 int Test1(int V, int itr, float P) {
 	int counter = 0; // counter for a number of connected graphs
@@ -235,11 +172,12 @@ int Test3(int V, int itr, float P) {
 int main() {
 	const int V = 1000; // number of vertices
 	const int itr = 500; // number of graphs
-	//float P100[10] = { 0.009, 0.01, 0.015, 0.02, 0.045, 0.05, 0.06, 0.07, 0.08, 0.09 };
-	float P1000[10] = { 0.0007, 0.002, 0.003,0.006, 0.0063,0.00699, 0.00788, 0.00999, 0.012, 0.014 }; // thershold 0.00690775527
-	//float P2_100[10] = { 0.5, 0.1, 0.15, 0.2, 0.25, 0.3, 0.5, 0.65, 0.8, 0.9 };
-	//float P2_1000[10] = { 0.01 , 0.05 , 0.07, 0.09, 0.01, 0.119, 0.13, 0.27, 0.48, 0.79 }; // threshold 0.11753940002
-
+	// thershold 0.00690775527
+	float P1000[10] = { 0.0007f, 0.002f, 0.003f,0.006f, 0.0063f,
+		0.00699f, 0.00788f, 0.00999f, 0.012f, 0.014f };
+	// threshold 0.11753940002
+	float P2_1000[10] = { 0.01f , 0.05f , 0.07f, 0.09f, 0.01f,
+		0.119f, 0.13f, 0.27f, 0.48f, 0.79f };
 	// opens an existing csv file or creates a new file.
 	ofstream file;
 	file.open("Test.csv");
@@ -252,13 +190,15 @@ int main() {
 	}
 	cout << "test1 is finished" << endl;
 
-	//file << "test2, Probability, diameter <= 2, diameter > 2" << endl;
-	//for (int i = 0; i < 10; i++) {
-	//	int test2 = Test2(V, itr, P2_1000[i]);
-	//	file << "," << P2_1000[i] << ":, " << test2 << ", " << (itr - test2) << endl;
-	//	cout << "P " << i + 1 << " finished in test2" << endl;
-	//}
-	//cout << "test2 is finished" << endl;
+
+	file << "test2, Probability, diameter <= 2, diameter > 2" << endl;
+	for (int i = 0; i < 10; i++) {
+		int test2 = Test2(V, itr, P2_1000[i]);
+		file << "," << P2_1000[i] << ":, " << test2 << ", " << (itr - test2) << endl;
+		cout << "P " << i + 1 << " finished in test2" << endl;
+	}
+	cout << "test2 is finished" << endl;
+
 
 	file << "test3, Probability, number of isolated vertices, number of non isolated vertices" << endl;
 	for (int i = 0; i < 10; i++) {
@@ -269,19 +209,5 @@ int main() {
 	cout << "test3 is finished" << endl;
 
 	file.close();
-	//int ok = 0;
-	/*for (int i = 0; i < 100; i++) {
-		Graph* graph1 = build_random_graph(1000, 0.012);
-		if (connectivity(graph1))
-			ok++;
-		delete[] graph1->adjList;
-		delete graph1;
-	}*/
-	//cout << ok;
-	//Graph* graph1 = build_random_graph(7, 0.5);
-	//printGraph(graph1);
-	//cout << is_isolated(graph1)<<endl;
-	//cout << diameter(graph1);
-	//create();
 	return 0;
 }
